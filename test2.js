b89f857e4c968670bddacaad60927d27ce3098ba
Alexander Gonzalez <julian1alex@gmail.com>
2019-10-28 09:26:22 -0700

    cards display

--- test2.js
+++ test2.js
@@ -32,27 +32,10 @@ $(document).ready(() => {
       console.log(cards);
       win++;
   
-      resetBoard();
-      console.log(win);
-      if (win === 9) {
-        $("#livesp").text("You Win!");
-        $("#livesWrapper").append(
-          "<br><button enabled class='play-again'>Play again?</button>"
-        );
-        start;
+      
       }
       $(".play-again").on("click", function() {
         location.reload(true);
       });
     }
-  
-    function unflipCards() {
-      lockBoard = true;
-  
-      setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-  
-    }
-    
-  })
+)
